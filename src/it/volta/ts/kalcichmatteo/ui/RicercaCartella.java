266c557b8f72ea924d40c61cbb6a441ea9270855
matteokalcich <matteokalcich@gmail.com>
2024-02-09 10:48:36 +0100

    optimized constrctor

--- src/it/volta/ts/kalcichmatteo/ui/RicercaCartella.java
+++ src/it/volta/ts/kalcichmatteo/ui/RicercaCartella.java
@@ -1,17 +1,10 @@
 package it.volta.ts.kalcichmatteo.ui;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class RicercaCartella extends JPanel {
@@ -19,41 +12,13 @@ public class RicercaCartella extends JPanel {
 	private JLabel filename;
 	private JTextField filepath;
 	private JButton fileChooserButton;
-	private JLabel fileField;
 	
 	private static final long serialVersionUID = 1L;
 
 	
 	public RicercaCartella() {
 		
-		this.setLayout(new BorderLayout());
-
-		
-		filename = new JLabel("Enter destination folder	");
-
-		this.add(filename, BorderLayout.NORTH);
-		
-		filepath = new JTextField("<path destination folder>");
-		this.add(filepath, BorderLayout.WEST);
-
-		fileField = new JLabel();
-
-        this.add(fileField, BorderLayout.CENTER);
-
-		fileChooserButton = new JButton("Search folder");
-
-        fileChooserButton.addActionListener((e) -> {
-            JFileChooser fileChooser = new JFileChooser();
-            int result = fileChooser.showOpenDialog(null);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                fileField.setText(fileChooser.getSelectedFile().getAbsolutePath());
-            }
-        });
-
-		this.add(fileChooserButton, BorderLayout.EAST);
-
-		
-
+		dirSearch();
 	}
 
 	public JPanel dirSearch() {
@@ -61,25 +26,18 @@ public class RicercaCartella extends JPanel {
 		this.setLayout(new BorderLayout());
 
 		
-		filename = new JLabel("Enter destination folder	");
+		filename = new JLabel("Enter destination folder");
 
 		this.add(filename, BorderLayout.NORTH);
 		
 		filepath = new JTextField("<path destination folder>");
 		this.add(filepath, BorderLayout.WEST);
 
-		fileField = new JLabel();
-
-        this.add(fileField, BorderLayout.CENTER);
-
 		fileChooserButton = new JButton("Search folder");
 
         fileChooserButton.addActionListener((e) -> {
-            JFileChooser fileChooser = new JFileChooser();
-            int result = fileChooser.showOpenDialog(null);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                fileField.setText(fileChooser.getSelectedFile().getAbsolutePath());
-            }
+            
+			new JFileChooser();
         });
 
 		this.add(fileChooserButton, BorderLayout.EAST);
