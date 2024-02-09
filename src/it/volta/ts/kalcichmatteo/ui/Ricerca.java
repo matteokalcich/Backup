edd0fdea933cae9f488e82a92749d07abf22f17f
matteokalcich <matteokalcich@gmail.com>
2024-02-09 10:52:56 +0100

    chenged method and split

--- src/it/volta/ts/kalcichmatteo/ui/Ricerca.java
+++ src/it/volta/ts/kalcichmatteo/ui/Ricerca.java
@@ -1,17 +1,23 @@
 package it.volta.ts.kalcichmatteo.ui;
 
+import java.awt.FlowLayout;
+
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 
 public class Ricerca {
 
     public Ricerca() {}
+    
+    public static JPanel ricerca() {
 
-    public static JSplitPane ricerca() {
+        JPanel pan = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
         JSplitPane split = new JSplitPane(JSplitPane.VERTICAL_SPLIT, new RicercaFile().fileSearch(), new RicercaCartella().dirSearch());
 
-        return split;
+        pan.add(split, FlowLayout.LEFT);
+
+        return pan;
     }
     
 }
