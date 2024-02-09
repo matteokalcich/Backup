2da9cf12bdc4720705a53b5425c6c93d2eb9571b
matteokalcich <matteokalcich@gmail.com>
2024-02-09 10:51:52 +0100

    optimized constructor

--- src/it/volta/ts/kalcichmatteo/ui/RicercaFile.java
+++ src/it/volta/ts/kalcichmatteo/ui/RicercaFile.java
@@ -1,16 +1,11 @@
 package it.volta.ts.kalcichmatteo.ui;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class RicercaFile extends JPanel {
@@ -18,38 +13,13 @@ public class RicercaFile extends JPanel {
 	private JLabel filename;
 	private JTextField filepath;
 	private JButton fileChooserButton;
-	private JLabel fileField;
 	
 	private static final long serialVersionUID = 1L;
 
 	
 	public RicercaFile() {
 		
-		this.setLayout(new BorderLayout());
-
-		
-		filename = new JLabel("Enter file name");
-
-		this.add(filename, BorderLayout.NORTH);
-		
-		filepath = new JTextField("<path file>");
-		this.add(filepath, BorderLayout.WEST);
-
-		fileField = new JLabel();
-
-        this.add(fileField, BorderLayout.CENTER);
-
-		fileChooserButton = new JButton("Cerca File");
-
-        fileChooserButton.addActionListener((e) -> {
-            JFileChooser fileChooser = new JFileChooser();
-            int result = fileChooser.showOpenDialog(null);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                fileField.setText(fileChooser.getSelectedFile().getAbsolutePath());
-            }
-        });
-
-		this.add(fileChooserButton, BorderLayout.EAST);
+		fileSearch();
 
 	}
 
@@ -62,21 +32,15 @@ public class RicercaFile extends JPanel {
 
 		this.add(filename, BorderLayout.NORTH);
 		
-		filepath = new JTextField("<path file>");
+		filepath = new JTextField("<path to the file>");
+		filepath.setMinimumSize(new Dimension(100, 100));
 		this.add(filepath, BorderLayout.WEST);
-
-		fileField = new JLabel();
-
-        this.add(fileField, BorderLayout.CENTER);
 
 		fileChooserButton = new JButton("Cerca File");
 
         fileChooserButton.addActionListener((e) -> {
-            JFileChooser fileChooser = new JFileChooser();
-            int result = fileChooser.showOpenDialog(null);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                fileField.setText(fileChooser.getSelectedFile().getAbsolutePath());
-            }
+            
+			new JFileChooser();
         });
 
 		this.add(fileChooserButton, BorderLayout.EAST);
