c875c19def7c9322ef1d40812db642e2eab89288
matteokalcich <matteokalcich@gmail.com>
2024-02-11 20:05:27 +0100

    Changed name from Schermata

---
+++ src/it/volta/ts/kalcichmatteo/ui/BackupUI.java
@@ -0,0 +1,25 @@
+package it.volta.ts.kalcichmatteo.ui;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class BackupUI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public BackupUI() {
+		
+		super("Backup");
+		this.setLayout(new BorderLayout());
+		this.setSize(800, 600);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.add(new Logo(), BorderLayout.NORTH);
+		//this.add(new JLabel("Benvenuti"), BorderLayout.AFTER_LINE_ENDS);
+		//this.add(new Welcome(), BorderLayout.NORTH);
+		this.add(Ricerca.ricerca(), BorderLayout.WEST);
+		this.setLocationRelativeTo(null);
+		//this.pack();
+		this.setVisible(true);
+	}
+}
