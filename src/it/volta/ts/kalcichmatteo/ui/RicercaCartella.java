e5ecdf49b58667cbdeed525c45d73ccf3a5bc292
MATTEO KALCICH <kalcich.matteo@volta.ts.it>
2024-02-22 15:52:47 +0100

    Added filechooser

--- src/it/volta/ts/kalcichmatteo/ui/RicercaCartella.java
+++ src/it/volta/ts/kalcichmatteo/ui/RicercaCartella.java
@@ -1,6 +1,8 @@
 package it.volta.ts.kalcichmatteo.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
@@ -10,7 +12,7 @@ import javax.swing.JTextField;
 public class RicercaCartella extends JPanel {
 
 	private JLabel filename;
-	private JTextField filepath;
+	private JLabel filepath;
 	private JButton fileChooserButton;
 	
 	private static final long serialVersionUID = 1L;
@@ -25,19 +27,23 @@ public class RicercaCartella extends JPanel {
 
 		this.setLayout(new BorderLayout());
 
-		
 		filename = new JLabel("Enter destination folder");
 
 		this.add(filename, BorderLayout.NORTH);
 		
-		filepath = new JTextField("<path destination folder>");
+		filepath = new JLabel("<path destination folder>");
+		filepath.setMinimumSize(new Dimension(100, 100));
 		this.add(filepath, BorderLayout.WEST);
 
 		fileChooserButton = new JButton("Search folder");
 
         fileChooserButton.addActionListener((e) -> {
             
-			new JFileChooser();
+        	JFileChooser fileChooser = new JFileChooser();
+            
+        	fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            int response = fileChooser.showOpenDialog(null);
+            
         });
 
 		this.add(fileChooserButton, BorderLayout.EAST);
