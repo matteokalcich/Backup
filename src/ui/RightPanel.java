902596edd697abe850cc1b5c9425861b45314316
matteokalcich <matteokalcich@gmail.com>
2024-03-12 19:16:15 +0100

    Aggiunto bottone e label

--- src/ui/RightPanel.java
+++ src/ui/RightPanel.java
@@ -1,8 +1,28 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class RightPanel extends JPanel {
+
+    private JButton deleteBtn;
+
+    private JLabel lb;
+
+    public RightPanel() {
+
+        this.setLayout(new BorderLayout());
+
+        deleteBtn = new JButton("Delete!");
+
+        this.add(deleteBtn, BorderLayout.NORTH);
+
+        this.lb = new JLabel();
+
+        this.lb.setText("Non so cosa fare");
+
+        this.add(lb, BorderLayout.CENTER);
+    }
 
 
 }
